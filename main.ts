f877f74c3b72c423a7aa1fe4ac5616d0c35dce3d
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-16 23:17:44 +0500

    main.ts

--- main.ts
+++ main.ts
@@ -1,65 +1,46 @@
 import inquirer from "inquirer";
-
-let input1 = await inquirer.prompt({
-    name: "username",
-    type: "input",
-    message: "Kindly, Enter your Name!"
-});
-
-let input2 = await inquirer.prompt({
-    name: "age",
+const input1 = await inquirer.prompt([
+  {
+    name: "num1",
     type: "number",
-    message: "Please, Enter your age!"
-});
+    message: "Enter your First Number:",
+  },
 
-if (input2.age >= 12) {
-    console.log(`${input1.username}, you can enjoy this game!!`);
+  {
+    name: "num2",
+    type: "number",
+    message: "Enter your Second Number:",
+  },
 
-    console.log("Now let's start the game!!");
-    let userGuess = await inquirer.prompt({
-        name: "userguess",
-        type: "number",
-        message: "Guess a Number!!"
-    });
+  {
+    name: "operator",
+    type: "list",
+    message: "Select one of the operators to perform action:",
+    choices: [
+      "addition",
+      "subtraction",
+      "Multiplication",
+      "Division",
+      "Modulus",
+    ],
+  },
+]);
 
-    let winningNumber = 38;
-    if (userGuess.userguess == winningNumber) {
-        console.log(`Wow ${input1.username}, you won this game.`);
-    } else {
-        if (userGuess.userguess > winningNumber) {
-            console.log(`${input1.username}, it's too high!`);
-        } else {
-            console.log(`${input1.username}, it's too low!`);
-        }
-    }
-} else {
-    let input3 = await inquirer.prompt({
-        name: "parentpermission",
-        type: "input",
-        message: "Parent, do you allow your kid to play this game? (yes/no)"
-    });
-
-    if (input3.parentpermission.toLowerCase() === "yes") {
-        console.log(`${input1.username}, enjoy your Game`);
-
-        console.log("Now let's start the game!!");
-        let userGuess = await inquirer.prompt({
-            name: "userguess",
-            type: "number",
-            message: "Guess a Number!!"
-        });
-
-        let winningNumber = 38;
-        if (userGuess.userguess == winningNumber) {
-            console.log(`Wow ${input1.username}, you won this game.`);
-        } else {
-            if (userGuess.userguess > winningNumber) {
-                console.log(`${input1.username}, it's too high!`);
-            } else {
-                console.log(`${input1.username}, it's too low!`);
-            }
-        }
-    } else {
-        console.log(`${input1.username}, sorry you can play others game.`);
-    }
+if (input1.operator === "addition") {
+  console.log(input1.num1 + input1.num2);
 }
+else if (input1.operator === "subtraction") {
+  console.log(input1.num1 - input1.num2);
+}
+else if (input1.operator === "Multiplication") {
+  console.log(input1.num1 * input1.num2);
+}
+else if (input1.operator === "Division") {
+  console.log(input1.num1 / input1.num2);
+}
+else if (input1.operator === "Modulus") {
+  console.log(input1.num1 % input1.num2);
+}
+else {
+  console.log("Invalid Operator");
+};
