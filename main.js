3081257922055a60377be26c4fe740c4c6a2ed12
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-18 02:01:32 +0500

    main.js

--- main.js
+++ main.js
@@ -25,20 +25,20 @@ const input1 = await inquirer.prompt([
     },
 ]);
 if (input1.operator === "addition") {
-    chalk.greenBright(console.log(input1.num1 + input1.num2));
+    console.log(chalk.greenBright(input1.num1 + input1.num2));
 }
 else if (input1.operator === "subtraction") {
-    chalk.greenBright(console.log(input1.num1 - input1.num2));
+    console.log(chalk.greenBright(input1.num1 - input1.num2));
 }
 else if (input1.operator === "Multiplication") {
-    chalk.greenBright(console.log(input1.num1 * input1.num2));
+    console.log(chalk.greenBright(input1.num1 * input1.num2));
 }
 else if (input1.operator === "Division") {
-    chalk.greenBright(console.log(input1.num1 / input1.num2));
+    console.log(chalk.greenBright(input1.num1 / input1.num2));
 }
-else if (input1.operator === "Modulus") {
-    chalk.greenBright(console.log(input1.num1 % input1.num2));
+else if (chalk.greenBright(input1.operator === "Modulus")) {
+    input1.num1 % input1.num2;
 }
 else {
-    chalk.greenBright(console.log("Please Select valid Operator"));
-}
+    console.log(chalk.red("Please Select valid Operator"));
+};
